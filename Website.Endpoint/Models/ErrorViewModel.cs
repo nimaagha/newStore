d26338f51e654513f67439319986c2476d91c94b
Nima Aghaei <aghaei_nima@yahoo.com>
2023-08-24 19:22:51 -0700

    Project first configs

---
+++ Website.Endpoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Website.Endpoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
